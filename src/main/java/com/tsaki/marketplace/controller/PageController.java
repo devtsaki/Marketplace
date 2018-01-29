860bdb4069b48504d4754c3f5e7138f899db9221
George Tsakiris <tsaki67@gmail.com>
2018-01-29 23:42:14 +0200

    Initial commit

---
+++ src/main/java/com/tsaki/marketplace/controller/PageController.java
@@ -0,0 +1,38 @@
+package com.tsaki.marketplace.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+
+	@RequestMapping(value= {"/", "home", "index"})
+	public ModelAndView index() {	
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greeting", "Welcome to tsaki Shop");
+		return mv;
+	}
+	
+//	@RequestMapping(value="/test")
+//	public ModelAndView test(@RequestParam(value = "greeting", required=false)String greeting) {
+//		if (greeting == null) {
+//			greeting = "default";
+//		}
+//		ModelAndView mv = new ModelAndView("page");
+//		mv.addObject("greeting", greeting);
+//		return mv;	
+//	}
+	
+	@RequestMapping(value="/test/{greeting}")
+	public ModelAndView test(@PathVariable("greeting")String greeting) {
+		if (greeting == null) {
+			greeting = "default";
+		}
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greeting", greeting);
+		return mv;	
+	}
+	
+}
